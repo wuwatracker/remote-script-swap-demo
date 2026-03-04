277b9117814da5a00d71bc7d9f4b4359d82aafe4
Luzefiru <105530193+Luzefiru@users.noreply.github.com>
2026-03-04 16:38:15 +0800

    feat: add rate limiter to prevent too much resource usage

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Hono } from "hono";
 import { CfBindings } from "./types";
+import { rateLimiter } from "hono-rate-limiter";
 
 const app = new Hono<{ Bindings: CfBindings }>();
+
+app.use(
+  rateLimiter<{ Bindings: CfBindings }>({
+    binding: (c) => c.env.RATE_LIMITER,
+    keyGenerator: (c) => c.req.header("cf-connecting-ip") ?? "",
+  }),
+);
 
 function flipACoin() {
   return Math.random() < 0.5 ? "Heads" : "Tails";
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 export type CfBindings = {
   MALICIOUS_SCRIPT_URL: `https://${string}`;
   SAFE_SCRIPT_URL: `https://${string}`;
+  RATE_LIMITER: RateLimit;
 };
